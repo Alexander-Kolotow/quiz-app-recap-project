3759101be58a148ced7cfa1a71ad3068323fa83f
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-10-24 17:53:23 +0000

    added toggle mode to bookmarks in JS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const bookmarkIcons = document.querySelectorAll('[data-js="bookmark"]');
 
+// Füge einen Klick-Ereignislistener zu jedem <i>-Element hinzu
+bookmarkIcons.forEach((icon) => {
+  icon.addEventListener("click", function () {
+    // Toggle die Klasse ".highlight" auf dem <i>-Element
+    icon.classList.toggle("highlight");
+  });
+});
