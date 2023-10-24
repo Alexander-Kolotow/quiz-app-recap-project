b965a03ff75d274114f1e6627ffc3e80bbc7d1db
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-10-24 09:55:14 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const bookmark = document.querySelector('[data-js="bookmark-icon"]');
+/*const bookmark = document.querySelector('[data-js="bookmark-icon"]');*/
+
 const toggleMode = document.querySelector('[data-js="toggle-bookmark"]');
 
 toggleMode.addEventListener("click", () => {
-  bookmark.classList.toggle("highlight");
+  toggleMode.classList.toggle("highlight");
 });
