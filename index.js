4c590a38b9c4d4f93a0a602ce8afee91ecda646c
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-10-25 10:59:48 +0000

    Update comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Es werden alle Elemente gefunden, die die Klasse "data-js="bookmark" enthalten.
 const bookmarkIcons = document.querySelectorAll('[data-js="bookmark"]');
 
-// Fügt ein Klick Event zu jedem <i>-Element (in der Funktion muss es ausgeschrieben werden <i> = icon) hinzu.
+// Fügt ein Klick Event zu jedem <i>-Element hinzu.
 bookmarkIcons.forEach((icon) => {
   icon.addEventListener("click", function () {
     // Toggle die Klasse ".highlight" auf den icon Elementen.
