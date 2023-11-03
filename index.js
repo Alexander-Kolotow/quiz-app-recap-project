040ad7b87a7e29cf813671287aaeb2b39708b645
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-11-03 13:27:01 +0000

    add form.html

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+// funktioniert so nicht, bisher nur Erfolg mit forEach-Schleife!
 const bookmarkIcon = document.querySelector('[data-js="bookmark"]');
 
 bookmarkIcon.addEventListener("click", function () {
   bookmarkIcon.classList.toggle("greenyellow");
+});
+
+const answerButton = document.querySelector(".answer-button");
+const answer = document.querySelector("p.hidden");
+
+answerButton.addEventListener("click", function () {
+  // Toggle the "hidden" class to display/hide the answer
+  answer.classList.toggle("hidden");
+
+  // Toggle the button text based on the answer's visibility
+  if (answer.classList.contains("hidden")) {
+    answerButton.textContent = "Show Answer";
+  } else {
+    answerButton.textContent = "Hide Answer";
+  }
 });
