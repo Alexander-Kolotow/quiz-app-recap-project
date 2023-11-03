fd142ae72e8b7f03cf177b9a237205b5410e0f0d
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-11-03 10:46:18 +0000

    update css

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-// Es werden alle Elemente gefunden, die die Klasse "data-js="bookmark" enthalten.
-const bookmarkIcons = document.querySelectorAll('[data-js="bookmark"]');
+const bookmarkIcon = document.querySelector('[data-js="bookmark"]');
 
-// Fügt ein Klick Event zu jedem <i>-Element hinzu.
-bookmarkIcons.forEach((icon) => {
-  icon.addEventListener("click", function () {
-    // Toggle die Klasse ".highlight" auf den icon Elementen.
-    icon.classList.toggle("highlight");
-  });
-});
-
-// Holen Sie sich die beiden Icon-Elemente
-const toggleOff = document.getElementById("toggle-off");
-const toggleOn = document.getElementById("toggle-on");
-
-// Fügen Sie einen Klick-Ereignishandler zu jedem Icon hinzu
-toggleOff.addEventListener("click", () => {
-  toggleOff.style.display = "none";
-  toggleOn.style.display = "inline-block";
-});
-
-toggleOn.addEventListener("click", () => {
-  toggleOn.style.display = "none";
-  toggleOff.style.display = "inline-block";
+bookmarkIcon.addEventListener("click", function () {
+  bookmarkIcon.classList.toggle("greenyellow");
 });
