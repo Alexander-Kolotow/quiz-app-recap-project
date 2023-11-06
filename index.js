dddc5cb9027faf343b798656337fa75c30396475
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-11-06 08:20:46 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Toggeln des ersten bookmarks.
 const bookmark = document.querySelector('[data-js="bookmark"]');
 
 bookmark.addEventListener("click", () => {
@@ -7,7 +8,7 @@ bookmark.addEventListener("click", () => {
     bookmark.classList.add("highlight");
   }
 });
-
+// toggeln des Show/Hide Buttons. Antwort wird sichtbar und textContent ändert sich beim Klicken.
 const answerButton = document.querySelector(".answer-button");
 const answer = document.querySelector("p.hidden");
 
@@ -17,5 +18,20 @@ answerButton.addEventListener("click", function () {
     answerButton.textContent = "Show Answer";
   } else {
     answerButton.textContent = "Hide Answer";
+  }
+});
+
+// Toggeln / Dark Mode.
+
+const toggleButton = document.querySelector('[data-js="toggle"]');
+const bodyElement = document.querySelector("body");
+
+toggleButton.addEventListener("click", function () {
+  if (body.classList.contains("darkgrey")) {
+    body.classList.remove("darkgrey");
+    toggleButton.textContent = "Dark mode off";
+  } else {
+    body.classList.add("darkgrey");
+    toggleButton.textContent = "Dark mode on";
   }
 });
